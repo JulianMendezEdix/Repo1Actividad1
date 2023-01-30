0ddd9d51afac599f737cbe1121ab12e127bb6d44
JulianMendez <JulianMendezEdix@gmail.com>
2023-01-30 17:53:05 +0100

    Requerimiento1_3a7 Añadida clase en java que imprime nombre y más

---
+++ Java/src/julian/Julian.java
@@ -0,0 +1,17 @@
+package julian;
+
+public class Julian {
+
+	public static void main(String[] args) {
+		
+		imprimirNombre ();
+
+	}
+
+	public static void imprimirNombre() {
+	
+		System.out.println("Hola, soy Julián");
+	
+	} 	
+	
+}
