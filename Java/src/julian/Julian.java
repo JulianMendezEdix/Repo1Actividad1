91b8521adea42d4bfc4e58ddc85b9af8b255d9c2
JulianMendez <JulianMendezEdix@gmail.com>
2023-02-03 11:22:36 +0100

    Requerimiento 3_2

--- Java/src/julian/Julian.java
+++ Java/src/julian/Julian.java
@@ -5,6 +5,8 @@ public class Julian {
 	public static void main(String[] args) {
 		
 		imprimirNombre ();
+		imprimirHobbies();
+		imprimirComidas();
 
 	}
 
@@ -17,6 +19,12 @@ public class Julian {
 	public static void imprimirHobbies() {
 		
 		System.out.println("Me gusta pasear, leer y ver pelis");
+	
+	} 	
+	
+	public static void imprimirComidas() {
+		
+		System.out.println("Mis comidas favoritas son la fabada, las lentejas y los macarrones");
 	
 	} 	
 	
