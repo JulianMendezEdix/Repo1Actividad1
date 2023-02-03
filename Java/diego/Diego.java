e2f0ad8b344469ea24a94ce81d44dd3cfa0f9411
DiegoEdix <digarciarodriguez@gmail.com>
2023-02-03 09:52:17 +0100

    Diego.class

---
+++ Java/diego/Diego.java
@@ -0,0 +1,17 @@
+package diego;
+
+public class Diego {
+
+	public static void main(String[] args) {
+		
+		imprimirNombre ();
+
+	}
+	
+	public static void imprimirNombre() {
+		
+		System.out.println("Hola, soy Diego");
+	
+	} 	
+
+}
